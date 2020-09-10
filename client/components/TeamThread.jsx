b63f795baf2039b33070b7b40693e92e5f7de068
SanjayLavingia <64515025+SanjayLavingia@users.noreply.github.com>
2020-09-10 16:46:54 -0700

    small changes

--- client/components/TeamThread.jsx
+++ client/components/TeamThread.jsx
@@ -1,10 +1,33 @@
 import React from 'React';
 
 function TeamThread(props) {
- 
+  const teamThreadStyles = {
+    padding: "50px",
+    backgroundColor: "#D3D3D3",
+    padding: "30px",
+    
+  }
+  const mystyle = {
+    color: "white",
+    backgroundColor: "DodgerBlue",
+    padding: "10px",
+    fontFamily: "Arial"
+  };
   const result = []; 
   for (let i = 0; i < props.teams.length; i++) {
-    result.push(<div key={`teamThread${i}`}> {`${props.teams[i].name} vs ${props.teams[i].opp} on ${props.teams[i].date} at ${props.teams[i].time}`} <button name={props.teams[i].name} onClick={props.remove}>Remove</button></div>)
+    result.push(<div
+      className="teamInfo"
+      style={teamThreadStyles} 
+      key={`teamThread${i}`}
+      > 
+      {`${props.teams[i].name} vs ${props.teams[i].opp} on ${props.teams[i].date} at ${props.teams[i].time}`} 
+      <button 
+      name={props.teams[i].name} 
+      onClick={props.remove}
+      >
+        Remove
+        </button>
+        </div>)
   }  
   console.log(props); 
   return <div>
