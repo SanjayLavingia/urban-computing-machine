42a59f467dfc053a6c2521a7c0d5363bd2ab0397
SanjayLavingia <64515025+SanjayLavingia@users.noreply.github.com>
2020-09-10 00:23:23 -0700

    database connected and working

--- server/routers/userRoutes.js
+++ server/routers/userRoutes.js
@@ -1,11 +1,22 @@
-const express = require('express')
+const express = require('express'); 
 const router = express.Router(); 
 
+const db = require('../models/userModel.js'); 
 
 
 router.get('/', (req, res) => {
-  //will get the current users list of teams from the db 
-  res.sendStatus(313); 
+  //will get the current users list of teams from the db
+  let sqlSelect = 'SELECT * FROM users';
+  db.query(sqlSelect)
+    .then(data => {
+      console.log(data);
+      res.status(248).json(data)
+    })
+    .catch(err => {
+      console.log(err)
+       res.sendStatus(500)
+    })
+    
 })
 
 router.post('/', (req, res) => {
