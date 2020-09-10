e7eea70340f7711efbf3cbc12a96ae96884e4258
SanjayLavingia <64515025+SanjayLavingia@users.noreply.github.com>
2020-09-10 09:15:30 -0700

    console logging CORS frontend request error

--- client/App.jsx
+++ client/App.jsx
@@ -5,6 +5,20 @@ import TeamThread from './components/TeamThread.jsx'
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state ={}; 
+
+    this.handleClick = this.handleClick.bind(this); 
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/user')
+      .then(res => res.json())
+      .then(user => {
+        console.log(user)
+        this.setState({
+        name: user
+      })})
+      .catch(err => console.log(err))
   }
 
   handleClick () {
